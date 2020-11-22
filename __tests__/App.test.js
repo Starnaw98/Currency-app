9fabb21ff30a8eb22d36a9d2be35b5e67b233019
Pawson199 <starnaw1998@interia.pl>
2020-11-22 19:48:33 +0100

    Format App tests

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -5,7 +5,7 @@ import App from '../src/App'
 import validNBP from '../__mocks__/validNBP.json'
 import { Provider } from "react-redux";
 import configureStore from 'redux-mock-store';
- 
+
 
 //Create fake store for Provider
 const mockStore = configureStore([]);
@@ -19,28 +19,26 @@ beforeEach(() => {
             filtered_currencies: []
         }
     });
-  });
+});
 
 //Mock fetch function and add ability to change second Resolver value.
 const mockFetch = (json) => {
-    global.fetch = jest.fn( () => Promise.resolve( json ) )
+    global.fetch = jest.fn(() => Promise.resolve(json))
 }
 
 
-it( 'load data on mount', async () => {
-
+it('fetch on mount succesfully', async () => {
     mockFetch({ json: () => Promise.resolve(validNBP) })
-    await act( async () => render( <Provider store={store} > <App />  </Provider> ) )
+    await act(async () => render(<Provider store={store} > <App />  </Provider>))
     expect(screen.getByText("Kantor")).toBeInTheDocument()
 })
 
 
-it( 'error after fetching', async () => {
-
+it('error after fetching', async () => {
     mockFetch({
         json: () => Promise.reject()
     })
-    await act( async () => render( <Provider store={store} > <App />  </Provider> ) )
+    await act(async () => render(<Provider store={store} > <App />  </Provider>))
     expect(screen.getByText("Przepraszamy, serwis chwilowo nieczynny.")).toBeInTheDocument()
 })
 
