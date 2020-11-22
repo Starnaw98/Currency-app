b247419318c7a4a57de080f55335f7c3b99be1ef
Pawson199 <starnaw1998@interia.pl>
2020-11-22 16:23:30 +0100

    Prevent App from running without fetching data
    If fetch in App component will return wrong data,
    App will show <h1>Service is down<h1/>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { connect } from "react-redux";
@@ -6,28 +6,42 @@ import Favorites from "./pages/Favorite";
 import Menu from "./components/Menu";
 import { saveCurrencies } from "./redux/reducers/allCurrencies";
 
-//After app reloading, save all currencies in  redux.
 function App(props) {
 
+  const [isFetched, setisFetched] = useState(false)
+  const [loadingInfo, setloadingInfo] = useState("Loading")
+
   useEffect(() => {
+
     fetch("http://api.nbp.pl/api/exchangerates/tables/c?format=json")
       .then((resp) => resp.json())
-      .then((resp) => props.saveCurrencies(resp[0].rates));
+      .then((resp) => {
+        props.saveCurrencies(resp[0].rates);
+        setisFetched(true)
+      })
+      .catch(() => {
+        setloadingInfo("Przepraszamy, serwis chwilowo nieczynny.");
+        new Error('Błąd podczas pobierania danych')
+      });
+
   }, []);
 
   return (
-    <>
-      <Menu />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-      </Switch>
-    </>
-  );
-}
+    isFetched ?
+      <>
+        <Menu />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+        </Switch>
+      </>
+      :
+      <h1> {loadingInfo} </h1>
 
+  )
+}
 export default connect(() => ({}), { saveCurrencies })(App);
