03649985f2c5c9f8f23908fe35b0e0680ce193f7
Pawson199 <starnaw1998@interia.pl>
2020-11-22 18:50:48 +0100

    Create fake store for App component Provider

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,9 +3,25 @@ import { render, screen, act } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from '../src/App'
 import validNBP from '../__mocks__/validNBP.json'
-import store from "../src/redux";
 import { Provider } from "react-redux";
+import configureStore from 'redux-mock-store';
+ 
 
+//Create fake store for Provider
+const mockStore = configureStore([]);
+let store;
+beforeEach(() => {
+    store = mockStore({
+        currencies: [],
+        exact_currency: {},
+        favorites: {
+            favorite_currencies: [],
+            filtered_currencies: []
+        }
+    });
+  });
+
+//Mock fetch function and add ability to change second Resolver value.
 const mockFetch = (json) => {
     global.fetch = jest.fn( () => Promise.resolve( json ) )
 }
